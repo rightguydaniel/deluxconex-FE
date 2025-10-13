0e10add987a4c547feeb1beba8097c0dfd0c4c2c
Rightguy <rightguydaniel@gmail.com>
2025-10-13 07:12:06 +0100

    fixed footer name

--- src/components/Homepage/Footer.tsx
+++ src/components/Homepage/Footer.tsx
@@ -152,7 +152,7 @@ export const Footer = () => {
           <a href="#" className="hover:text-blue-500">
             Sitemap
           </a>
-          <span>© 2025 Conexwest. All rights reserved.</span>
+          <span>© 2025 Deluxconex. All rights reserved.</span>
         </div>
       </div>
     </div>
